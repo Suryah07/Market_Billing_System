619e8b6733b6b17ac5dbeeca1f7516ea5d89af07
Suryah07 <suryahdhamodharan@gmail.com>
2021-08-29 22:45:07 +0530

    The output of the bill has been added and can be succesfully seen after adding each items

--- Layout.py
+++ Layout.py
@@ -9,13 +9,18 @@ title.pack(fill = X)
 
 #==============PRODUCT DETAILS==================
 
-#add the item to the bill
+#add the item to the bill and give output of the bill
 def additem():
-    print(typed_text)
+    #print(typed_text)
     #product.delete(1,END)
-
-
-
+    bill_list.append(typed_text)
+    print(bill_list)
+    i = additem.counter
+    bill_box.insert(0,bill_list[i])
+    print(i)
+    additem.counter = additem.counter+1
+    
+additem.counter = 0
 
 # Update the listbox
 def update_listbox(data):
@@ -51,22 +56,32 @@ def check(e):
   # update our listbox
   update_listbox(data)        
 
-# Create a label
+# Create a label for search
 Searchlab = Label(root, text="Search",font=("times new romman", 14))
 Searchlab.place(x=400,y=120)
+#Create a label for quantity
 Quantitylab = Label(root, text="Quantity",font=("times new romman", 14))
 Quantitylab.place(x=900,y=120)
 
-# Create an entry box
+# Create an entry box for product
 product = Entry(root, font=("Helvetica"),width=70)
 product.place(x=60,y=150)
 
+#Create an entry box for quantity
 quantity = Entry(root,font=("Helvetica"),width=10)
 quantity.place(x=900,y=150)
 
 # Create a listbox
 list_box = Listbox(root, width=100)
 list_box.place(x=60,y=190)
+
+#Creating a list to store the items in the bill
+bill_list =[]
+
+#Creating a list box display the bill
+bill_box = Listbox(root,width=100,height=30,)
+bill_box.place(x=800,y=190)
+#bill_box.insert(1,"Suryah")
 
 # Programming Language List
 programming_lan = ["Python","Javascript","C++","Java","PHP","Kotlin","Suryah","apple","suryah12","adventure","windows","macos"]
@@ -80,6 +95,7 @@ list_box.bind("<<ListboxSelect>>", update)
 # Create a binding on the entry box
 product.bind("<KeyRelease>", check)
 
+#Adding a button to add the selected item to the bill of the customer
 addbutton = Button(text="Add Item",command=additem)
 addbutton.place(x=1050,y=150)
 
