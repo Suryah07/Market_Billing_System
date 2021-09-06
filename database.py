9b5ce8666630b28083905831e56fed60959a74a3
Suryah07 <suryahdhamodharan@gmail.com>
2021-09-06 13:51:28 +0530

    Relative imports

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
 import config
 import mysql.connector
+from .main import Layout
+
 con = mysql.connector.connect(host=config.host,user=config.username,password=config.userpass,database=config.database)
